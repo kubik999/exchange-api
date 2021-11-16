bdf882ff6d9e8addd4670f31ad40cf74b181fe42
Kuba <jakub.przygodzki@student.wat.edu.pl>
2021-11-16 09:24:54 +0100

    v3

--- src/main/java/pl/app/exchange/NbpUsdV1.java
+++ src/main/java/pl/app/exchange/NbpUsdV1.java
@@ -2,8 +2,8 @@ package pl.app.exchange;
 
 import lombok.*;
 
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 
 @Getter
 @Setter
@@ -15,19 +15,19 @@ public class NbpUsdV1 {
     private String table;
     private String currency;
     private String code;
-    private ArrayList<LinkedHashMap<String, Object>> rates;
+    private List<LinkedHashMap<String, Object>> rates;
 
     public double getBid() {
         return this.getRates().stream()
                 .findFirst()
                 .map(obj -> (Double) obj.get("bid"))
-                .orElseThrow(() -> new RuntimeException("Brak wartości ceny usdy"));
+                .orElseThrow(() -> new RuntimeException("Brak wartości ceny usd"));
     }
 
     public double getAsk() {
         return this.getRates().stream()
                 .findFirst()
                 .map(obj -> (Double) obj.get("ask"))
-                .orElseThrow(() -> new RuntimeException("Brak wartości ceny usdy"));
+                .orElseThrow(() -> new RuntimeException("Brak wartości ceny usd"));
     }
 }
